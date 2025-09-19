135ddd7e75a55f6bf363267f33dfc57718520301
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-19 16:47:31 +0300

    createemployee function created

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function createEmployeeRecord([firstName, familyName, title, payHour]){
+    return {
+        firstName: "",
+        familyName: "",
+        title: "",
+        payHour: "",
+        timeInEvents: [],
+        timeOutEvents: [],}
+}
+
